6ee0db7439c0cd32248ad57e7acb9f00bb115a67
Thomas Ballinger <thomasballinger@gmail.com>
2012-12-17 16:18:30 -0500

    refactor waiting for boolean in client

--- client.py
+++ client.py
@@ -9,26 +9,21 @@ class Client(object):
         self.s = socket.socket()
         self.s.connect((ADDRESS, PORT))
         self.ready_for_word = False
-    def play(self):
+    def wait_for(self, query, message='waiting...'):
         while True:
-            self.s.send('paired?')
+            self.s.send(query)
             m = self.s.recv(1000)
             if m != 'False':
                 print m
                 break
             time.sleep(1)
-            print 'waiting for partner...'
+            print message
+    def play(self):
+        self.wait_for('paired?', 'waiting for partner...')
         word = raw_input('Enter a word for your opponent to guess: ')
         self.s.send('assign '+word)
         m = self.s.recv(1000)
-        while True:
-            self.s.send('ready?')
-            m = self.s.recv(1000)
-            if m != 'False':
-                print m
-                break
-            time.sleep(1)
-            print 'waiting for partner to give us a word'
+        self.wait_for('ready?', 'waiting for partner to give us a word...')
         while True:
             guess = raw_input('Enter a letter to guess: ')
             self.s.send('guess '+guess)
